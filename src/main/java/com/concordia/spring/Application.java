f285cc8cd19018584add2eb274fd9b6b42dc8bf4
mayankvadariya <48036907+mayankvadariya@users.noreply.github.com>
2020-04-29 15:52:16 -0400

    Update Application.java

--- src/main/java/com/concordia/spring/Application.java
+++ src/main/java/com/concordia/spring/Application.java
@@ -3,6 +3,7 @@ package com.concordia.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //This is the init class
+//This is second line comment
 @SpringBootApplication
 public class Application {
 
